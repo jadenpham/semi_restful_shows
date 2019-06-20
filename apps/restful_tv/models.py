b72bcc338e66d7e83c091def2c4c71f0249cb409
Jaden Pham <tjaden.pham@gmail.com>
2019-06-20 15:41:41 -0700

    second commit with validation

--- apps/restful_tv/models.py
+++ apps/restful_tv/models.py
@@ -1,4 +1,19 @@
+from __future__ import unicode_literals
 from django.db import models
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors={}
+        title = Show.objects.filter(title=postData['title'])
+        if len(title) > 0:
+            errors['title'] = "Show already exist"
+        elif len(postData['title'])<2:
+            errors['title'] = "Show title should be at least 2 characters"
+        elif len(postData['network'])<3:
+            errors['network'] = "Show's network should be at least 3 characters"
+        elif len(postData['description'])<10:
+            errors['description'] = "Show's description must be at least 10 characters"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +22,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- apps/restful_tv/views.py
+++ apps/restful_tv/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from . import models
+from .models import Show
 
 def index(request):
     context={
@@ -11,7 +13,13 @@ def new_show(request):
     return render(request, 'restful_tv/addnewshow.html')
 
 def create(request):
-    show_info = models.Show.objects.create(title=request.POST['title'], network=request.POST['network'], released_date=request.POST['release_date'], description=request.POST['description'])
+    errors= Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        show_info = models.Show.objects.create(title=request.POST['title'], network=request.POST['network'], released_date=request.POST['release_date'], description=request.POST['description'])
     print(show_info, "show info")
     return redirect("/shows/"+str(show_info.id)) #redirecting to url with show.id in it, can't pass context over
 
@@ -31,15 +39,18 @@ def edit(request, show_id):
     return render(request, 'restful_tv/editshow.html', context)
 
 def update(request, show_id):
-    update_show=models.Show.objects.get(id=show_id)
-    update_show.title=request.POST['u_title']
-    update_show.save()
-    update_show.network=request.POST['u_network']
-    update_show.save()
-    update_show.released_date=request.POST['u_release_date']
-    update_show.save()
-    update_show.description=request.POST['u_description']
-    update_show.save()
+    errors= Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/'+ str(show_id) + '/edit')
+    else:
+        update_show=models.Show.objects.get(id=show_id)
+        update_show.title=request.POST['title']
+        update_show.network=request.POST['network']
+        update_show.released_date=request.POST['release_date']
+        update_show.description=request.POST['description']
+        update_show.save()
     return redirect ('/shows/'+str(show_id))
 
 def delete(request, show_id):
